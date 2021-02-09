ecaef8f590f454dc593e673811144a44cfda766a
Temyper <pall47g1@gmail.com>
2021-02-10 02:30:21 +0900

    [Add] a function download  valid CSV files with japanese.

--- js/body.js
+++ js/body.js
@@ -1,22 +1,41 @@
+// 20210209TODO:CSVファイルの入力
+
 "use strict";
 
-$("#output-csv").click(function (e) {
+$("#output-csv__english").click(function (e) {
+  // 英語オンリー
   let originString =
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, voluptate non. Rerum labore tenetur itaque eveniet iure. Perferendis odio, quos deleniti eius vero ducimus architecto suscipit fugit officiis sed culpa.";
 
   let text = originString.replace(/ /g, ",");
 
-  let content = new Blob([text], { type: "text" });
+  let content = new Blob([text], { type: "text/csv" });
 
   let newDownloader = document.createElement("a");
   document.body.appendChild(newDownloader);
   // 20210207 非推奨らしいttps://www.tcmobile.jp/dev_blog/programming/webrtc-%E3%81%A7-url-createobjecturl%E3%81%AF%E3%81%BE%E3%82%82%E3%81%AA%E3%81%8F%E4%BD%BF%E3%81%88%E3%81%AA%E3%81%8F%E3%81%AA%E3%82%8B/
   newDownloader.href = URL.createObjectURL(content);
-  newDownloader.download = "test.txt";
+  newDownloader.download = "test.csv";
 
-  // let newDownloader = $('<a id="download-maker">');
-  // 20210208↓とりあえず以前からのやり方を採用するので見送り
-  // newDownloader.srcObject = content;
+  newDownloader.click();
+  document.body.removeChild(newDownloader);
+  e.preventDefault();
+});
+
+// 20210210日本語はBOM付きで出力しないと文字化けする
+$("#output-csv__japanese").click(function (e) {
+  var bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+  let originString = "あ い う え お";
+
+  let text = originString.replace(/ /g, ",");
+
+  let content = new Blob([bom, text], { type: "text/csv" });
+
+  let newDownloader = document.createElement("a");
+  document.body.appendChild(newDownloader);
+  // 20210207 非推奨らしいttps://www.tcmobile.jp/dev_blog/programming/webrtc-%E3%81%A7-url-createobjecturl%E3%81%AF%E3%81%BE%E3%82%82%E3%81%AA%E3%81%8F%E4%BD%BF%E3%81%88%E3%81%AA%E3%81%8F%E3%81%AA%E3%82%8B/
+  newDownloader.href = URL.createObjectURL(content);
+  newDownloader.download = "test.csv";
 
   newDownloader.click();
   document.body.removeChild(newDownloader);
