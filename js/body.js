aca0e9ce7ebaaabbc4ba57bd60a9fd2e472854a4
Temyper <pall47g1@gmail.com>
2021-02-11 09:52:57 +0900

    [Add] loading a text file.

--- js/body.js
+++ js/body.js
@@ -4,7 +4,6 @@
 
 // 20210210BOM無しver
 $("#output-csv__english-without-bom").click(function (e) {
-  // 英語オンリー
   let originString =
     "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, voluptate non. Rerum labore tenetur itaque eveniet iure. Perferendis odio, quos deleniti eius vero ducimus architecto suscipit fugit officiis sed culpa.";
 
@@ -13,8 +12,9 @@ $("#output-csv__english-without-bom").click(function (e) {
   let content = new Blob([text], { type: "text/csv" });
 
   let newDownloader = document.createElement("a");
+
   document.body.appendChild(newDownloader);
-  // 20210207 非推奨らしいttps://www.tcmobile.jp/dev_blog/programming/webrtc-%E3%81%A7-url-createobjecturl%E3%81%AF%E3%81%BE%E3%82%82%E3%81%AA%E3%81%8F%E4%BD%BF%E3%81%88%E3%81%AA%E3%81%8F%E3%81%AA%E3%82%8B/
+
   newDownloader.href = URL.createObjectURL(content);
   newDownloader.download = "test.csv";
 
@@ -24,8 +24,7 @@ $("#output-csv__english-without-bom").click(function (e) {
 });
 
 // 20210210BOM付き。日本語混在対策にBOM付けるのをデフォルトにしよう
-$("#output-csv__english-with-bom").click(function (e) {
-  // 英語オンリー
+$("#output-csv__english-and-japanese-with-bom").click(function (e) {
   let originString =
     "Lorem ipsum dolor あ 唖 ア ああ sit amet consectetur, い adipisicing elit. Aut, voluptate non. Rerum labore tenetur itaque eveniet iure. Perferendis odio, quos deleniti eius vero ducimus architecto suscipit fugit officiis sed culpa.";
 
@@ -36,8 +35,9 @@ $("#output-csv__english-with-bom").click(function (e) {
   let content = new Blob([bom, text], { type: "text/csv" });
 
   let newDownloader = document.createElement("a");
+
   document.body.appendChild(newDownloader);
-  // 20210207 非推奨らしいttps://www.tcmobile.jp/dev_blog/programming/webrtc-%E3%81%A7-url-createobjecturl%E3%81%AF%E3%81%BE%E3%82%82%E3%81%AA%E3%81%8F%E4%BD%BF%E3%81%88%E3%81%AA%E3%81%8F%E3%81%AA%E3%82%8B/
+
   newDownloader.href = URL.createObjectURL(content);
   newDownloader.download = "test.csv";
 
@@ -56,12 +56,18 @@ $("#output-csv__japanese").click(function (e) {
   let content = new Blob([bom, text], { type: "text/csv" });
 
   let newDownloader = document.createElement("a");
+
   document.body.appendChild(newDownloader);
-  // 20210207 非推奨らしいttps://www.tcmobile.jp/dev_blog/programming/webrtc-%E3%81%A7-url-createobjecturl%E3%81%AF%E3%81%BE%E3%82%82%E3%81%AA%E3%81%8F%E4%BD%BF%E3%81%88%E3%81%AA%E3%81%8F%E3%81%AA%E3%82%8B/
+
   newDownloader.href = URL.createObjectURL(content);
   newDownloader.download = "test.csv";
 
   newDownloader.click();
   document.body.removeChild(newDownloader);
+  e.preventDefault();
+});
+
+$("#input-csv__text-file-all").click(function (e) {
+  $("#input-csv__text-file-all").load("../resource/test.csv");
   e.preventDefault();
 });
